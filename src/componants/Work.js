035fe021654187fb8e5d1790c3fdf2b4f9b17525
Mohamed <114217025+melhelow@users.noreply.github.com>
2023-06-12 18:39:55 -0500

    align descroption right of the image

--- src/componants/Work.js
+++ src/componants/Work.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import proj1Image from "../images/plant-pedia-logo.png"
-// import myImage from "../images/profile.svg"
 
-function Work(props) {
+function Work() {
 
     const myProjects = [
         {
@@ -16,46 +14,42 @@ function Work(props) {
             discreption: "Kidding Kittys is a web application that allows users to search for cats and get information about them. The user can also save cats to their favorites list and view them later. The user can also view the weather in their area and get a list of cats that are best suited for the current weather conditions.",
         },
 
-        // {
-        //     title: "sample",
-        //     image: "https://placehold.co/600x400"
-        // },
-        // {
-        //     title: "sample",
-        //     image: "https://placehold.co/600x400"
-        // }
     ]
 
     return (
-        <div className="container flex flex-row justify-center items-stretch  ">
-            <div className="m-5 border-r-2 border-black p-5  ">
+        <div className="container flex    ">
+            <div className="m-5  border-r-2 border-black p-5   ">
                 <h1>Work</h1>
             </div>
-            {/* <div></div> */}
+          
             <div className="m-5 ">
                 
                {
                 myProjects.map(project => {
                     return (
-                        <>
+                        <div className='flex flex-row'>
+                            <div className='flex flex-col  '>
                         <h1>{project.title}</h1>
                         
                         
                         <div
-                        className='h-96 w-96 m-5  bg-cover bg-center shadow-2xl shadow-emerald-950   '
+                        className=' flex flex-row h-96 w-96 m-5  bg-cover bg-center shadow-2xl shadow-emerald-950   '
                         
                             style={{
                                 backgroundImage: `url(${project.image}) `,
                             }}
                         >
-                           
-
+                               
+                                </div>
                         </div>
-                        <div className='m-5 '>
+                                <div className='m-5 flex items-center '>
                             
-                            <p>{project.discreption}</p></div>
-                        
-                        </>
+                            <p>{project.discreption}</p>
+                            </div>
+                        </div>
+
+                      
+                   
                     )
                 })
                }
