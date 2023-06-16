75bc9429560dcd4d7213c6eb00f3f930cf508475
Mohamed <114217025+melhelow@users.noreply.github.com>
2023-06-15 22:43:53 -0500

    changed avatar location from navbar to about me

--- src/componants/About.js
+++ src/componants/About.js
@@ -1,13 +1,22 @@
 import React from 'react';
-
+import avatar from '../images/me 3.jpg'
 function About() {
     return (
-        <div className="container flex flex-row  font-normal  ">
-            <div className="m-5 border-r-2 border-black p-5 px-20 ">
+        <div className="container flex flex-row  font-normal   ">
+            <div className="m-5 border-r-2 border-black p-5 px-20  ">
                 <h1>About Me</h1>
             </div>
-            <div className="m-5  decoration-double text-justify  text-lg text-center w-1/2 pt-2">
+            <div className="m-5  flex flex-row decoration-double text-justify  text-lg text-center w-1/2 pt-2 ">
                 <p>Full stack web developer with a background in Business & Economics. Trained at Northwestern coding boot camp and earned a certificate in full-stack web development. Innovative problem-solver passionate about developing apps; focused on mobile-first design and development. Strengths in creativity, teamwork, and building projects from ideation to execution.</p>
+                
+                     
+            </div>
+            <div className='flex justify-center  p-8 '>
+                   <img
+                        className=" h-48 w-48 rounded-full"
+                        src={avatar}
+                        alt=""
+                      />  
             </div>
             
             
--- src/componants/Navbar.js
+++ src/componants/Navbar.js
@@ -1,7 +1,7 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import avatar from '../images/image.png'
+
 
 
 
@@ -54,62 +54,12 @@ export default function Example(props) {
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
       
-
-                {/* Profile dropdown */}
-                <Menu as="div" className="relative ml-3">
-                  <div>
-                    <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                      <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
+              {/* <img
+                        className="h-40 w-40 rounded-full"
                         src={avatar}
                         alt=""
                       />
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Your Profile
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Settings
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Sign out
-                          </a>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
+                */}
               </div>
             </div>
           </div>
