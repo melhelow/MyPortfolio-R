02b1d3a74354e680b36f4224e0b452ff869377e1
Mohamed <114217025+melhelow@users.noreply.github.com>
2023-06-28 13:17:49 -0500

    updated resume.pdf

--- src/componants/About.js
+++ src/componants/About.js
@@ -8,7 +8,7 @@ function About() {
             </div>
             <div className="m-5  flex flex-row decoration-double text-justify  text-lg text-center w-1/2 pt-2 ">
                 <p>Full stack web developer with a background in Business & Economics. Trained at Northwestern coding boot camp and earned a certificate in full-stack web development. Innovative problem-solver passionate about developing apps; focused on mobile-first design and development. Strengths in creativity, teamwork, and building projects from ideation to execution.</p>
-                
+                    
                      
             </div>
             <div className='flex justify-center  p-8 '>
